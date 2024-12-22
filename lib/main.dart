726a4d7e3125f39f9bf453339d46eab623a3cda9
JHProyectos <129443750+JHProyectos@users.noreply.github.com>
2024-12-22 09:23:26 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'src/core/providers/theme_provider.dart';
-import 'src/core/providers/locale_provider.dart';
-import 'src/core/pages/settings_page.dart'; // Cambia según tu proyecto
-import 'generated/l10n.dart'; // Generado por intl para la localización
+import 'src/core/navigation/routes.dart'; // Importa tu archivo de rutas
+import 'src/core/pages/settings_page.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
+  runApp(
+    EasyLocalization(
+      supportedLocales: const [Locale('en'), Locale('es')],
+      path: Routes.translationPath, // Usando la ruta definida en routes.dart
+      fallbackLocale: const Locale('en'),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => ThemeProvider()),
-        ChangeNotifierProvider(create: (_) => LocaleProvider()),
       ],
-      child: Consumer2<ThemeProvider, LocaleProvider>(
-        builder: (context, themeProvider, localeProvider, child) {
+      child: Consumer<ThemeProvider>(
+        builder: (context, themeProvider, child) {
           return MaterialApp(
             debugShowCheckedModeBanner: false,
             theme: themeProvider.currentTheme,
-            locale: localeProvider.locale,
-            supportedLocales: const [
-              Locale('en'),
-              Locale('es'),
-            ],
-            localizationsDelegates: const [
-              S.delegate, // Delegado generado automáticamente
-              GlobalMaterialLocalizations.delegate,
-              GlobalWidgetsLocalizations.delegate,
-              GlobalCupertinoLocalizations.delegate,
-            ],
-            home: const SettingsPage(), // Página de configuración o inicial
+            locale: context.locale,
+            supportedLocales: context.supportedLocales,
+            localizationsDelegates: context.localizationDelegates,
+            home: const SettingsPage(),
           );
         },
       ),
