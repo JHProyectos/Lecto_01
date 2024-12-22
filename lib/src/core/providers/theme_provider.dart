7badd567f51450fc96b801c9dd7b6436b1720e79
JHProyectos <129443750+JHProyectos@users.noreply.github.com>
2024-12-22 08:56:02 -0300

    Create theme_provider.dart

---
+++ lib/src/core/providers/theme_provider.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ThemeProvider with ChangeNotifier {
+  bool _isDarkTheme = false;
+
+  bool get isDarkTheme => _isDarkTheme;
+
+  void toggleTheme() {
+    _isDarkTheme = !_isDarkTheme;
+    notifyListeners();
+  }
+
+  ThemeData get currentTheme => _isDarkTheme ? ThemeData.dark() : ThemeData.light();
+}
